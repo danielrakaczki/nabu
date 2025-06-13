5e22e1ac859fce794bb14cacb6845b489f4fcdf3
danielrakaczki <daniel.rakaczki@gmail.com>
2025-06-13 19:06:37 +0200

    package updates

--- openapi-config.cjs
+++ openapi-config.cjs
@@ -1,22 +1,22 @@
-require('dotenv-flow').config();
+require("dotenv-flow").config();
 
 const baseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 if (!baseUrl) {
-  throw new Error('Missing EXPO_PUBLIC_SUPABASE_URL in environment');
+  throw new Error("Missing EXPO_PUBLIC_SUPABASE_URL in environment");
 }
 
 module.exports = {
-  schemaFile: `${baseUrl}/rest/v1/?openapi`,
-  apiFile: './lib/emptyApi.ts',
-  apiImport: 'emptySplitApi',
-  outputFile: './lib/generatedApi.ts',
-  exportName: 'generatedApi',
+  schemaFile: `https://mnpporglidnwszwmwjth.supabase.co/rest/v1/?openapi`,
+  apiFile: "./lib/emptyApi.ts",
+  apiImport: "emptySplitApi",
+  outputFile: "./lib/generatedApi.ts",
+  exportName: "generatedApi",
   hooks: true,
   httpResolverOptions: {
     headers: {
       apiKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       Authorization: `Bearer ${process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY}`,
-      Accept: 'application/json',
-    }
-  }
-}
+      Accept: "application/json",
+    },
+  },
+};
